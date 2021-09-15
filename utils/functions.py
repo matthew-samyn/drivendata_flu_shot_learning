617f19124e0a8ba5997ab3db7e464e8ede01abc3
matthew-samyn <84380381+matthew-samyn@users.noreply.github.com>
2021-09-15 16:59:36 +0200

    Made better models, new DF's, new analysis

--- utils/functions.py
+++ utils/functions.py
@@ -1,6 +1,10 @@
-from typing import List
+from typing import List,Tuple
 import pandas as pd
 import numpy as np
+import os
+import re
+
+
 
 def seperate_columns_by_type(df_series:pd.Series, types:List) -> bool:
     """ Seperates dataframe columns into categorical and numerical """
@@ -8,6 +12,7 @@ def seperate_columns_by_type(df_series:pd.Series, types:List) -> bool:
         return True
     else:
         return False
+
 
 def category_to_binary(x:str, category_name:str) -> int:
     """ """
@@ -19,9 +24,71 @@ def category_to_binary(x:str, category_name:str) -> int:
     else:
         return 1
 
+
 def dummies_into_dataframe(df: pd.DataFrame, column:str) -> pd.DataFrame:
+    """ Uses pd.get_dummies on column.
+    Automatically merges it back with the original DataFrame.
+
+    :return original DataFrame with added dummie columns.
+    """
     df_with_dummies = pd.get_dummies(df[column])
     df = df.drop([column], axis=1)
     df = pd.merge(df, df_with_dummies, left_index=True, right_index=True)
     return df
+
+
+def get_score(h5_file:str) -> Tuple[float, float]:
+    """ Extracts useful data from a .h5 file
+
+    :param h5_file: File to extract the train and val accuracy scores from
+    :return: train accuracy, difference between train and validation
+    """
+    scores = re.findall(r"\d+\.\d+",h5_file)
+    train_accuracy = float(scores[0])
+    val_accuracy = float(scores[1])
+    difference = train_accuracy - val_accuracy
+    return val_accuracy, difference
+
+
+def keep_best_saved_h5(folder_relative:str, common_filename: str, maximum_difference:float) -> str:
+    """ Goes through all common named .h5-files,
+    deletes all from folder except for the best result.
+
+    :param maximum_difference: max difference allowed between accuracy and val_accuracy.
+    :return Best scoring .h5 file
+    """
+    current_directory = os.getcwd()
+    os.chdir(current_directory + folder_relative)
+    all_files = os.listdir()
+    best_scoring_file = ""
+    # try-except incase of errors: returns to current directory
+    try:
+        # Keep the files with a low difference between train_accuracy and validation_accuracy.
+        # Deletes the rest from directory
+        model_files = [file for file in all_files if file.startswith(common_filename)]
+        scores = []
+        not_overfitting_models = []
+        best_scoring_file = ""
+        for file in model_files:
+            validation_accuracy, diff = get_score(file) # Uses function get_score()
+            if abs(diff) > maximum_difference:
+                os.remove(file)
+            else:
+                not_overfitting_models.append(file)
+                scores.append(validation_accuracy)
+
+        # Keep only the file with highest validation accuracy score.
+        # Deletes the rest from directory
+        highest_score_index = scores.index(max(scores))
+        for i, file in enumerate(not_overfitting_models):
+            if i == highest_score_index:
+                best_scoring_file = file
+            else:
+                os.remove(file)
+        os.chdir(current_directory)
+    except:
+        os.chdir(current_directory)
+    print(f"Currently in directory:{os.getcwd()}")
+    print(f"File coming out of the function: {best_scoring_file}")
+    return best_scoring_file
 
--- utils/preprocessing_data.py
+++ utils/preprocessing_data.py
@@ -8,7 +8,10 @@ df = pd.read_csv("../data/training_set_features.csv")
 target = pd.read_csv("../data/training_set_labels.csv")
 
 full_df = pd.merge(df, target, on="respondent_id")
-print(full_df.isna().sum())
+
+full_df.to_csv("../data/untouched_with_labels.csv", index=False)
+
+
 # 2-category columns to binary column
 full_df["sex"] = df["sex"].apply(lambda x: category_to_binary(x,"Male"))
 full_df["marital_status"] = df["marital_status"].apply(lambda x:
@@ -17,6 +20,7 @@ full_df["marital_status"] = df["marital_status"].apply(lambda x:
 full_df["rent_or_own"] = df["rent_or_own"].apply(lambda x:
                                             category_to_binary(x, "Rent"))
 # get_dummies on multiple-category-columns
+
 types = ["int64","float64"]
 categorical = []
 numerical = []
@@ -28,10 +32,22 @@ for column in full_df.columns:
 
 for column in categorical:
     full_df = dummies_into_dataframe(full_df, column)
+print(full_df.dtypes)
 
 # Creating seperate dataframe for each model
 df_h1n1 = full_df.drop(["seasonal_vaccine"], axis=1)
 df_seasonal = full_df.drop(["h1n1_vaccine"], axis=1)
+
+# After exploring the data, decided on what columns could get dropped
+columns_to_drop_h1n1 = ["behavioral_antiviral_meds","behavioral_large_gatherings", "behavioral_outside_home",
+                        "opinion_seas_risk", "opinion_seas_sick_from_vacc", "opinion_seas_vacc_effective",
+                        "household_adults", "household_children", "doctor_recc_seasonal", "opinion_h1n1_sick_from_vacc"]
+
+columns_to_drop_seasonal = ["doctor_recc_h1n1", "child_under_6_months", "opinion_h1n1_vacc_effective", "opinion_h1n1_risk",
+                            "opinion_h1n1_sick_from_vacc","opinion_seas_sick_from_vacc","household_adults","household_children"]
+
+df_h1n1 = df_h1n1.drop(columns_to_drop_h1n1, axis=1)
+df_seasonal = df_seasonal.drop(columns_to_drop_seasonal, axis=1)
 
 # Creating csv for each model
 df_h1n1.to_csv("../data/training_h1n1.csv", index=False)
